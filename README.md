0a2f08324a1f44fb514613f6c9a6006c62de90a0
Florian.S <florian0410@hotmail.fr>
2015-06-18 22:57:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Android Arsenal](https://img.shields.io/badge/Android%20Arsenal-Radial%20Button%20Layout-brightgreen.svg?style=flat)](https://android-arsenal.com/details/3/1142)
+
+# Radial Button Layout
+
+This example project illustrates how to implement an animated radial button layout in Android.
+
+![alt tag](https://cloud.githubusercontent.com/assets/885861/5096537/b0fa6334-6f7b-11e4-8b01-61f731793a97.gif)
---
+++ app/src/main/java/com/mitchwongho/radialbuttons/MainActivity.java
@@ -0,0 +1,27 @@
+package com.mitchwongho.radialbuttons;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText( this, R.string.click_the_button, Toast.LENGTH_LONG).show();
+    }
+}
